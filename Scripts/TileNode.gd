3887780dcab57772ca7b74318a20622b158f5a02
Erlend Solerød <34630777+ErlandSol@users.noreply.github.com>
2025-01-05 21:21:42 +0100

    yes

--- Scripts/TileNode.gd
+++ Scripts/TileNode.gd
@@ -6,8 +6,7 @@ var direction: Util.Direction
 
 
 
-# direction is relative to the board, the node tree doesnt care about
-# rotation or visuals, only data
+# direction is relative to the tile
 var child = {
 	top = null,
 	bottom = null,
