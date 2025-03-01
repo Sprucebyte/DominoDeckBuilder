6aba4658776cc81f8141134dad74031c21b60ef7
Hans <111863567+HansPluss@users.noreply.github.com>
2025-03-01 02:33:08 +0100

    Added some more wildcards
    + Added code for Perfect square
    + Added code for baby steps

---
+++ Scripts/Wild Cards/Baby_steps.gd
@@ -0,0 +1,15 @@
+extends WildCard
+
+func activate() -> void:
+	super()
+	var activateSpeed = 1
+	for node: TileNode in GameManager.board.tileNodeTree.getEdgeNodes():
+		var tile = node.tile
+		var value = node.getEdgeValue()
+		if not (roundi(value) < 4): continue
+		await get_tree().create_timer(.2/ GameManager.gameSpeedMultiplier / activateSpeed).timeout
+		SignalBus.AddToScore.emit(2)
+		tile.shake()
+		ScoreLabel.Spawn(tile,"+" + str(2), Color.BLUE)
+		activateSpeed *= 1.05
+	pass
