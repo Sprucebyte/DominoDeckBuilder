f36a25ebeb27c8f6df7a506f840c6d7e41b4aedc
Hans <111863567+HansPluss@users.noreply.github.com>
2025-03-24 13:21:49 +0100

    Changed gravity wildcard
    + Updated description

--- Scripts/Wild Cards/Gravity.gd
+++ Scripts/Wild Cards/Gravity.gd
@@ -1,9 +1,14 @@
 extends WildCard
 
 var lastTotalvalue = 0
+var hasNine = false
+var hasEight = false
+var hasOne = false
 #gives +9.81 mult + it accelerates after each use, giving a little bonus
 func activate() -> bool:
 	super ()
+	addMult(9.81)
+	shake()
 	var totalValue = 0
 	for node: TileNode in edgeNodes():
 		var tile = node.tile
@@ -12,10 +17,20 @@ func activate() -> bool:
 		
 		
 	var stringValue = str(totalValue)
-	if stringValue.contains("9") and stringValue.contains("8") and stringValue.contains("1"):
+	if stringValue.contains("9"):
+		hasNine = true
+	if stringValue.contains("8"):
+		hasEight = true
+	if stringValue.contains("1"):
+		hasOne = true
+	
+	if hasEight and hasNine and hasOne:
 		var bonus = 9.81 + (lastTotalvalue * 0.0981)
 		lastTotalvalue = totalValue
 		addMult(bonus)
 		shake()
+		hasOne = false
+		hasNine = false
+		hasEight = false
 		return true
 	return false
